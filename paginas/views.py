080c6ff050e03849c666ea4c40909740d9a3add8
Vítor Lessa <vitorlcaraujo@gmail.com>
2020-10-17 15:49:59 -0300

    segundo commit

---
+++ paginas/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Importa o TemplateView para criar paginas simples
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+# A classe PaginaInicial "estends" TemplateView
+class IndexView(TemplateView):
+  # Toda classe filha do TemplateView precisa do
+  # atributo abaixo para definir um template a ser renderizado
+  template_name = 'paginas/index.html'
+
+# class SobreView(TemplateView):
+#   template_name = 'paginas/sobre.html'
+
